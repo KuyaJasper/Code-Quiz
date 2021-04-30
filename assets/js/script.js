0e0074fe94f87df6fb5a13d86e8a1f89fbaa07f4
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-30 18:49:44 -0400

    refractored and de bugged javascript file. Set up event listener for buttons, created forloop for game questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,18 +48,28 @@ var currentQuestion3 = questions[count].choices[2];
 var currentQuestion4 = questions[count].choices[3];
 
 
-// function checkAnswer(Question) {
-//     console.log(Question)
-//     console.log(questions[count].answer)
-//     console.log(Question === questions[count].answer)
-//     if (Question === questions[count].answer) {
-//         ++count;
-//         ++score;
-//     }
-//     else {
-//         --score;
-//     }
-// }
+// Checking answers for event listener which is codeded into the HTML
+function checkAnswer(event) {
+    console.log(event.target.textContent)
+    console.log(questions[count].answer)
+    console.log(event.target.textContent === questions[count].answer)
+    if (event.target.textContent === questions[count].answer) {
+        score++;
+    }
+    else {
+        score--;
+        gameTime-= 10;
+    }
+
+    count++;
+
+    if (count == questions.length) {
+        alert("Game Over")
+        // set up in game function here later.
+    }else {
+        gameQuestions();
+    }
+}
 
 function gameTimer() {
   // Sets interval in variable
@@ -76,6 +86,22 @@ function gameTimer() {
   }, 1000);
 }
 
+function gameQuestions (){
+    title.textContent = questions[count].title;
+    for (var i = 0; i<4; i++){
+        if(i === 0){
+    choiceOne.textContent = questions[count].choices[i];
+        }else if(i === 1){
+            choiceTwo.textContent = questions[count].choices[i];
+        }else if (i=== 2){
+            choiceThree.textContent = questions[count].choices[i];
+        }else {
+            choiceFour.textContent = questions[count].choices[i];
+        }
+    }
+    
+}
+
 startQuiz.addEventListener("click", function () {
     gameTimer();
     console.log(questions);
@@ -85,15 +111,11 @@ startQuiz.addEventListener("click", function () {
     document.getElementsByClassName('startcard')[0].classList.add('hide');
     document.getElementById('startQuiz').classList.add('hide');
 
-    title.textContent = questions[count].title;
-    choiceOne.textContent = currentQuestion1;
-    choiceTwo.textContent = currentQuestion2;
-    choiceThree.textContent = currentQuestion3;
-    choiceFour.textContent = currentQuestion4;
+gameQuestions();
 
-    // choiceOne.addEventListener("click", function () { checkAnswer(currentQuestion1) });
-    // choiceTwo.addEventListener("click", function () { checkAnswer(currentQuestion2) });
-    // choiceThree.addEventListener("click", function () { checkAnswer(currentQuestion3) });
-    // choiceFour.addEventListener("click", function () { checkAnswer(currentQuestion4) });
-    // console.log(score);
+    // choiceOne.addEventListener("click", function () { checkAnswer() });
+    // choiceTwo.addEventListener("click", function () { checkAnswer() });
+    // choiceThree.addEventListener("click", function () { checkAnswer() });
+    // choiceFour.addEventListener("click", function () { checkAnswer() });
+    console.log(score);
 });
