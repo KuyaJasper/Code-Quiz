d5f7556c25159dfb96ff26f78d420903103c3f08
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-06 15:15:41 -0400

    started high scores html and js

---
+++ assets/js/highscore.js
@@ -0,0 +1,18 @@
+let highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+let scoreList = documentgetElementbyID("scoreLIst");
+let clearButton = documentgetElementbyID("clearButton");
+
+highScores.sort(function (a,b) {
+    return b.score - a.score
+})
+
+for (var i=0; i <highScores.length; i++) {
+    var newLi = document.createElement("li")
+    newLi.textContent = highScores[i].name + "-" + highScores[i].score
+    scoreList.apendChild(newLi)
+}
+
+clearButton.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.reload();
+});
