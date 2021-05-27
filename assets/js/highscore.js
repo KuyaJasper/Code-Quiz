2fc00c28cbce58a604e40086fed708eb1451bbbf
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-27 18:05:45 -0400

    fixed logic for local storage

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -6,10 +6,16 @@ var StartOver = document.getElementById("newQuiz");
 renderUserScore();
 
 function renderUserScore() {
+//GOAL: Create a new element, have it set both, append child.
     var finalscore = localStorage.getItem("PlayerScore");
     var finalname = localStorage.getItem("userName");
-    userName.textContent = finalname;
-    userScore.textContent = finalscore;
+    // userName.textContent = finalname;
+    // userScore.textContent = finalscore;
+    var currentScore = document.querySelector("#scoreList");
+    var newScoreContainer = document.createElement('div');
+    var newScore = document.createTextNode(`Name: ${finalname} | Score: ${finalscore}`);
+    newScoreContainer.appendChild(newScore);
+    currentScore.appendChild(newScoreContainer);
 }
 
 
@@ -19,9 +25,8 @@ clearButton.addEventListener("click", function () {
     window.location.reload();
 });
 
-StartOver.addEventListener("click", function () {
+StartOver.addEventListener("click", function (event) {
     window.location.replace("./index.html");
 });
 
 
-// TO DO: re structure local storage to JSON objects????
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,46 @@
 //Questions for Quiz
 var questions = [
-    {//Question 1
-        title: "Which of the following tags creates a button in HTML:",
-        choices: ["<br>", "<footer></footer>", "<div></div>", "<button></button>"],
-        answer: "<button></button>"
-    },
+  {
+    //Question 1
+    title: "Which of the following tags creates a button in HTML:",
+    choices: ["<br>", "<footer></footer>", "<div></div>", "<button></button>"],
+    answer: "<button></button>",
+  },
 
-    {//Question 2
-        title: "Which one of these is NOT a commonly used datatype in Javascript:",
-        choices: ["strings", "booleans", "beans", "numbers"],
-        answer: "beans"
-    },
+  {
+    //Question 2
+    title: "Which one of these is NOT a commonly used datatype in Javascript:",
+    choices: ["strings", "booleans", "beans", "numbers"],
+    answer: "beans",
+  },
 
-    {// Question 3
-        title: "Inside which HTML element do we put JavaScript into?",
-        choices: ["<div>", "<javascript>", "<script>", "<js>"],
-        answer: "<script>"
-    },
+  {
+    // Question 3
+    title: "Inside which HTML element do we put JavaScript into?",
+    choices: ["<div>", "<javascript>", "<script>", "<js>"],
+    answer: "<script>",
+  },
 
-    {//Question 4
-        title: "Choose the Javascript syntax that will create a function called: CodingisEasy.",
-        choices: ["function CodingisEasy()", "function:CodingisEasy()", "function = #CodingisEasy()", "function(CodingisEasy)"],
-        answer: "function CodingisEasy()"
-    },
+  {
+    //Question 4
+    title:
+      "Choose the Javascript syntax that will create a function called: CodingisEasy.",
+    choices: [
+      "function CodingisEasy()",
+      "function:CodingisEasy()",
+      "function = #CodingisEasy()",
+      "function(CodingisEasy)",
+    ],
+    answer: "function CodingisEasy()",
+  },
 
-    {//Question 5
-        title: "Which string method combines two or more strings together to create a new string?",
-        choices: ["combine()", "concat()", "toLowerCase()", "length()"],
-        answer: "concat()"
-    },
-
-
+  {
+    //Question 5
+    title:
+      "Which string method combines two or more strings together to create a new string?",
+    choices: ["combine()", "concat()", "toLowerCase()", "length()"],
+    answer: "concat()",
+  },
 ];
 
 // Javascript for Game itself
@@ -50,129 +60,132 @@ var currentQuestion3 = questions[count].choices[2];
 var currentQuestion4 = questions[count].choices[3];
 var submitButton = document.getElementById("submitButton");
 var PlayerFinalScore = document.getElementById("PlayerFinalScore");
-var startOverBtn= document.querySelector("#newQuiz");
-
-
-
+var startOverBtn = document.querySelector("#newQuiz");
 
 // Checking answers for event listener which is codeded into the HTML
 function checkAnswer(event) {
-    scoreEL.textContent = score;
-    PlayerFinalScore.textContent = score;
-    console.log("your choice is:" + event.target.textContent)
-    console.log("the correct answer is:" + questions[count].answer)
-    // console.log(event.target.textContent === questions[count].answer)
-    if (event.target.textContent === questions[count].answer) {
-        score+=100;
-        gameTime+=10;
-    }
-    else {
-        score-=100;
-        gameTime-= 10;
-    }
+  scoreEL.textContent = score;
+  PlayerFinalScore.textContent = score;
+  console.log("your choice is:" + event.target.textContent);
+  console.log("the correct answer is:" + questions[count].answer);
+  // console.log(event.target.textContent === questions[count].answer)
+  if (event.target.textContent === questions[count].answer) {
+    score += 100;
+    gameTime += 10;
+  } else {
+    score -= 100;
+    gameTime -= 10;
+  }
 
-    count++;
+  count++;
 
-    if (count == questions.length) {
-        endGame();
-        // set up in game function here later. reset the game, or send you to highscores HTML
-    }else {
-        gameQuestions();
-    }
+  if (count == questions.length) {
+    endGame();
+    // set up in game function here later. reset the game, or send you to highscores HTML
+  } else {
+    gameQuestions();
+  }
 }
 
 function gameTimer() {
-var countdown = setInterval(function() {
+  var countdown = setInterval(function () {
     timeEl.textContent = gameTime;
     gameTime--;
-    if(gameTime === 0){
-        gameTimeStop(countdown);
-      }else if (gameTime < 0){
-          gameTimeStop(countdown);
-          timeEl.textContent= 0;
-          scoreEL.textContent = score;
-          endGame();
-      }
+    if (gameTime === 0) {
+      gameTimeStop(countdown);
+    } else if (gameTime < 0) {
+      gameTimeStop(countdown);
+      timeEl.textContent = 0;
+      scoreEL.textContent = score;
+      endGame();
+    }
   }, 1000);
 }
 function gameTimeStop(interval) {
-    let gameTime = 0;
-    timeEl.textContent = gameTime;
-    clearInterval(interval);
+  let gameTime = 0;
+  timeEl.textContent = gameTime;
+  clearInterval(interval);
 }
 
-function gameQuestions (){
-    scoreEL.textContent = score;
-    PlayerFinalScore.textContent = score;
-    title.textContent = questions[count].title;
-    for (var i = 0; i<4; i++){
-        if(i === 0){
-    choiceOne.textContent = questions[count].choices[i];
-        }else if(i === 1){
-            choiceTwo.textContent = questions[count].choices[i];
-        }else if (i=== 2){
-            choiceThree.textContent = questions[count].choices[i];
-        }else {
-            choiceFour.textContent = questions[count].choices[i];
-        }
-    }
-    
-}
-
-function endGame (){
-    PlayerFinalScore.textContent = score;
-    gameTimeStop();
-    document.getElementById('finalScore').classList.remove('hide');
-    document.getElementById('finalScore').classList.add('show');
-    document.getElementsByClassName('choices')[0].classList.remove('show');
-    document.getElementsByClassName('choices')[0].classList.add('hide');
-    document.getElementById('card-question').classList.add('hide');
-    document.getElementById('cardScore').classList.remove('hide');
-    timeEl.classList.add('hide');
-    scoreEL.classList.add('hide');
-    
-}
-
-function localStorageData (userData){
-
-    // TODO FIX LOCAL STORAGE LOCATIONS/FUNCTIONS
-    let userHistory;
-    if (localStorage.getItem("History") === null) {
-      userHistory = [];
+function gameQuestions() {
+  scoreEL.textContent = score;
+  PlayerFinalScore.textContent = score;
+  title.textContent = questions[count].title;
+  for (var i = 0; i < 4; i++) {
+    if (i === 0) {
+      choiceOne.textContent = questions[count].choices[i];
+    } else if (i === 1) {
+      choiceTwo.textContent = questions[count].choices[i];
+    } else if (i === 2) {
+      choiceThree.textContent = questions[count].choices[i];
     } else {
-      userHistory = JSON.parse(localStorage.getItem("History",PlayerScore, userName));
+      choiceFour.textContent = questions[count].choices[i];
     }
-    var PlayerScore = score;
-var userName = document.querySelector("#userName").value;
-    userHistory.push(userData);
-    localStorage.setItem("History", JSON.stringify(PlayerScore, userName));
-    localStorage.setItem("PlayerScore", PlayerScore);
-localStorage.setItem("userName", userName);
+  }
+}
+
+function endGame() {
+  PlayerFinalScore.textContent = score;
+  gameTimeStop();
+  document.getElementById("finalScore").classList.remove("hide");
+  document.getElementById("finalScore").classList.add("show");
+  document.getElementsByClassName("choices")[0].classList.remove("show");
+  document.getElementsByClassName("choices")[0].classList.add("hide");
+  document.getElementById("card-question").classList.add("hide");
+  document.getElementById("cardScore").classList.remove("hide");
+  timeEl.classList.add("hide");
+  scoreEL.classList.add("hide");
+}
+
+function localStorageData() {
+  var playerScore = score;
+  var userName = document.querySelector("#userName").value.trim();
+
+  var gameScore = {
+      name: userName,
+      score: playerScore
+  };
+
+  console.log(gameScore);
+
+  var userHistory = JSON.parse(localStorage.getItem('userHistory') || "[]");
+  //   let userHistory;
+  // if (localStorage.getItem("userHistory") === null) {
+  //   userHistory = [];} 
+  //   else {
+  //   userHistory = JSON.parse(localStorage.getItem("userHistory"));
+  // }
+
+  userHistory.push(gameScore);
+
+  // first paramater is the key name, second paramter is the value. in local storage
+
+  localStorage.setItem('userHistory', JSON.stringify(userHistory));
 }
 
 startQuiz.addEventListener("click", function () {
-    scoreEL.textContent = score;
-    timeEl.textContent = gameTime;
-    gameTimer();
-    gameQuestions();
-    document.getElementsByClassName('choices')[0].classList.remove('hide');
-    document.getElementsByClassName('choices')[0].classList.add('show');
-    document.getElementsByClassName('startcard')[0].classList.add('hide');
-    document.getElementById('startQuiz').classList.add('hide');
+  scoreEL.textContent = score;
+  timeEl.textContent = gameTime;
+  gameTimer();
+  gameQuestions();
+  document.getElementsByClassName("choices")[0].classList.remove("hide");
+  document.getElementsByClassName("choices")[0].classList.add("show");
+  document.getElementsByClassName("startcard")[0].classList.add("hide");
+  document.getElementById("startQuiz").classList.add("hide");
 });
 
-submitButton.addEventListener("click", function(event) {
-event.preventDefault();
-window.location.replace("./highscores.html");
-pageloadFunction();
+submitButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    localStorageData()
+  window.location.replace("./highscores.html");
+//   pageloadFunction();
 });
 
-
-function pageloadFunction() {
-    let userHistory;
-    if (localStorage.getItem("History") === null) {
-      userHistory = [];
-    } else {
-      userHistory = JSON.parse(localStorage.getItem("History"));
-    }
-  }
+// function pageloadFunction() {
+//   let userHistory;
+//   if (localStorage.getItem("History") === null) {
+//     userHistory = [];} 
+//     else {
+//     userHistory = JSON.parse(localStorage.getItem("History"));
+//   }
+// }
