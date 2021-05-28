8530dd65dfe604cc109b91dbe59116bced1540bf
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-27 21:30:29 -0400

    Fixed logic in highsore.js to show new line items in a for loop

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,26 +1,23 @@
-var userName = document.getElementById('userName');
-var userScore = document.getElementById("userScore");
 var clearButton = document.getElementById("clearButton");
 var StartOver = document.getElementById("newQuiz");
+var scoreList = document.querySelector('#scoreList');
+var userHistory = JSON.parse(localStorage.getItem('userHistory') || "[]");
+
+
+userHistory.sort(function (a,b){
+    return b.score - a.score
+})
 
 renderUserScore();
 
 function renderUserScore() {
-//GOAL: Create a new element, have it set both, append child.
-    var userHistory = JSON.parse(localStorage.getItem('userHistory') || "[]");
-    var finalscore = JSON.parse(localStorage.getItem('userHistory'));
-    var finalname = userHistory.name;
-    console.log(userHistory[0].name);
-    console.log(userHistory[0].score);
 
-    // userName.textContent = finalname;
-    // userScore.textContent = finalscore;
+    for (var i=0; i< userHistory.length; i++){
+        var newScore = document.createElement('li');
+        newScore.textContent= `Name: ${userHistory[i].name} || Score: ${userHistory[i].score}`
+        scoreList.appendChild(newScore)
+    }
 
-    // var currentScore = document.querySelector("#scoreList");
-    // var newScoreContainer = document.createElement('div');
-    // var newScore = document.createTextNode(`Name: ${finalname} | Score: ${finalscore}`);
-    // newScoreContainer.appendChild(newScore);
-    // currentScore.appendChild(newScoreContainer);
 }
 
 
